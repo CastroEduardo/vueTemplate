6c6065419fd8575dca04046eda3f7ef210d675d8
Castrox220 <castro2354@gmail.com>
2021-09-15 11:19:04 -0400

    Update 2 x220

--- src/views/pages/login/Login.vue
+++ src/views/pages/login/Login.vue
@@ -38,7 +38,7 @@
               <div class="px-8 pt-8 login-tabs-container">
                 <div class="vx-card__title mb-4">
                   <h4 class="mb-4">Login</h4>
-                  <p>Welcome back, please login to your account.</p>
+                  <p>Welcome back, please login to your account need.</p>
                 </div>
 
                 <vs-tabs>
